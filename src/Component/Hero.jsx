35bf0c41d75a519473cbc6a7bdc38cac009110c4
a-k-a-s-h-12 <90305231+a-k-a-s-h-12@users.noreply.github.com>
2025-12-06 14:14:18 +0530

    Update Hero.jsx

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { HERO_CONTENT } from "../constants";
 import profile2 from "../assets/profile2.jpeg";
-import naukriprofile from "../assets/projects/naukriprofile.jpeg";
+import Profile from "../assets/projects/Final_profile.JPG';
 import { motion } from "framer-motion";
 const container = (delay) => ({
   hidden: { x: -100, opacity: 0 },
@@ -45,7 +45,7 @@ const Hero = () => {
               initial={{ x: 100, opacity: 0 }}
               animate={{ x: 1, opacity: 1 }}
               transition={{ duration: 0.5, delay: 1.2 }}
-              src={naukriprofile}
+              src={Profile}
               alt="profile pic"
               className="w-[400px] h-[450px] rounded-lg brightness-75"
             />
